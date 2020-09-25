b5cf46682a6e443438f36fbf42f4035e3ececfb8
Godelvatai <godelvatai@gmail.com>
2020-09-25 15:36:04 -0400

    Drawcut on z and x
    Working version of basic cutting on both the x and z axes. Cuts are currently happening seperate and not cutting proper grid.

--- DrawCut.cs
+++ DrawCut.cs
@@ -6,6 +6,7 @@ public class DrawCut : MonoBehaviour
 {
     Vector3 pointA;
     Vector3 pointB;
+    Vector3 pointC;
     
     Camera cam;
     public GameObject obj;
@@ -16,22 +17,46 @@ public class DrawCut : MonoBehaviour
 
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        mouse.z = -cam.transform.position.z;
-
         if (Input.GetMouseButtonDown(0)) {
-            pointA = cam.ScreenToWorldPoint(mouse);
-            
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+			    obj = hit.collider.gameObject; 
+			}
         }
-        if (Input.GetMouseButtonUp(0)) {
-            pointB = cam.ScreenToWorldPoint(mouse);
-            CreateSlicePlane();
+        
+        if (Input.GetKeyDown("space")) 
+        {
+            //float xIndex = obj.GetComponent<MeshFilter>().mesh.bounds.size.x * obj.transform.localScale.x;
+            //float zIndex = obj.GetComponent<MeshFilter>().mesh.bounds.size.z * obj.transform.localScale.z;
+           
+            float xIndex = -5;
+            for(int i=0; i<3; i++)
+            {
+                pointA = new Vector3(xIndex,0,1);
+                pointB = new Vector3(xIndex,1,0);
+                pointC = new Vector3(xIndex,0,0);
+                CreateSlicePlane();
+                xIndex += 5;
+            }
+        }
+        if (Input.GetKeyDown("z"))
+        {
+            float zIndex = 5;
+            for(int i=0; i<3; i++)
+            {
+                pointA = new Vector3(1,0,zIndex);
+                pointB = new Vector3(0,1,zIndex);
+                pointC = new Vector3(0,0,zIndex);
+                CreateSlicePlane();
+                zIndex -= 5;
+            }
         }
     }
 
     void CreateSlicePlane() {
         Vector3 centre = (pointA+pointB)/2;
-        Vector3 up = Vector3.Cross((pointA-pointB),(pointA-cam.transform.position)).normalized;
+        Vector3 up = Vector3.Cross((pointB-pointA),(pointC-pointA)).normalized;
         
         
         Cutter.Cut(obj, centre, up,null,true,true);
